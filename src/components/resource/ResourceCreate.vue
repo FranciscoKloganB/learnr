c4a5aac6550ecf22adbb44889c6029a921a0e60b
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2021-10-04 14:27:10 +0100

    Rename vue-first-app to learnr

--- src/components/resource/ResourceCreate.vue
+++ src/components/resource/ResourceCreate.vue
@@ -1,64 +1,67 @@
 <template>
-<div class="column is-half">
-  <form>
-    <div class="field">
-      <label class="label">Title</label>
-      <div class="control">
-        <input
-          v-model="form.title"
-          class="input"
-          type="text"
-          placeholder="Provide a short description or name for your resource."
-        />
+  <div class="column is-half">
+    <form>
+      <div class="field">
+        <label class="label">Title</label>
+        <div class="control">
+          <input
+            v-model="form.title"
+            class="input"
+            type="text"
+            placeholder="Provide a short description or name for your resource."
+          />
+        </div>
+        <p v-show="errors.title" class="help is-danger">
+          Name field must have at most 32 characters. E.g., Fast API
+        </p>
       </div>
-      <p v-show="errors.title" class="help is-danger">
-        Name field must have at most 32 characters. E.g., Fast API
-      </p>
-    </div>
 
-    <div class="field">
-      <label class="label">Link</label>
-      <div class="control">
-        <input
-          v-model="form.link"
-          class="input"
-          type="url"
-          placeholder="Where in the web can we find more information about the resource."
-        />
+      <div class="field">
+        <label class="label">Link</label>
+        <div class="control">
+          <input
+            v-model="form.link"
+            class="input"
+            type="url"
+            placeholder="Where in the web can we find more information about the resource."
+          />
+        </div>
+        <p v-show="errors.link" class="help is-danger">
+          Link must be a valid web address. E.g., https://fastapi.tiangolo.com
+        </p>
       </div>
-      <p v-show="errors.link" class="help is-danger">
-        Link must be a valid web address. E.g., https://fastapi.tiangolo.com
-      </p>
-    </div>
 
-    <div class="field">
-      <label class="label">Description</label>
-      <div class="control">
-        <textarea
-          v-model="form.description"
-          class="textarea"
-          placeholder="Provide a lengthy description for the resource. E.g., what is the resource about?"
-        ></textarea>
+      <div class="field">
+        <label class="label">Description</label>
+        <div class="control">
+          <textarea
+            v-model="form.description"
+            class="textarea"
+            placeholder="Provide a lengthy description for the resource. E.g., what is the resource about?"
+          ></textarea>
+        </div>
+        <p v-show="errors.description" class="help is-danger">
+          A resource description must be at least 64 characters long.
+        </p>
       </div>
-      <p v-show="errors.description" class="help is-danger">
-        A resource description must be at least 64 characters long.
-      </p>
-    </div>
 
-    <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-link" v-on:click.prevent="createResource">
-          Submit
-        </button>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link" v-on:click.prevent="createResource">
+            Submit
+          </button>
+        </div>
+        <div class="control">
+          <button
+            class="button is-link is-light"
+            v-on:click.prevent="clearForm"
+          >
+            Cancel
+          </button>
+        </div>
       </div>
-      <div class="control">
-        <button class="button is-link is-light" v-on:click.prevent="clearForm">
-          Cancel
-        </button>
-      </div>
-    </div>
-  </form>
-</div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -79,11 +82,10 @@ const toastOptions = {
   closeButton: 'button',
   icon: true,
   rtl: false
-}
+};
 
-const error = (message) => toast.error(message, toastOptions);
-const info = (message) => toast.info(message, toastOptions);
-const success = (message) => toast.success(message, toastOptions);
+const error = message => toast.error(message, toastOptions);
+const success = message => toast.success(message, toastOptions);
 
 export default {
   components: {},
@@ -110,7 +112,7 @@ export default {
     },
     createResource() {
       if (this.errorsExist()) {
-        error('Can not create a resource until all form errors are corrected.')
+        error('Can not create a resource until all form errors are corrected.');
       } else {
         this.onCreateResourceItem({
           ...this.form,
@@ -135,7 +137,7 @@ export default {
         description: '',
         link: ''
       };
-    },
+    }
   }
 };
 </script>
